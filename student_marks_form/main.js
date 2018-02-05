22af077fc82db8635f63bc27a1c19190477422f1
Anusha <anushakurra509@gmail.com>
2018-02-05 12:17:55 -0800

    added studentname to form,

--- student_marks_form/main.js
+++ student_marks_form/main.js
@@ -1,4 +1,8 @@
 function validateMarksForm(marksFormObj) {
+    if(marksFormObj.name.value==""){
+        alert("Please enter name");
+        return false;
+    }
     var length = marksFormObj.marks.length;
     for (i = 0; i < length; i++) {
         if (marksFormObj.marks[i].value == "") {
