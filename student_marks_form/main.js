edf750c697d21eb090e2be95d39de71b86a43c55
Anusha <anushakurra509@gmail.com>
2018-02-05 13:17:48 -0800

    added alert msg with marks, chnged the fontweight for labels...

--- student_marks_form/main.js
+++ student_marks_form/main.js
@@ -4,12 +4,17 @@ function validateMarksForm(marksFormObj) {
         return false;
     }
     var length = marksFormObj.marks.length;
+    var alertMessage = "";
     for (i = 0; i < length; i++) {
         if (marksFormObj.marks[i].value == "") {
             alert("Please enter marks");
             marksFormObj.marks[i].focus();
             return false;
+        } else {
+            alertMessage = alertMessage + "\n" + marksFormObj.marks[i].value;
         }
     }
+    alert(alertMessage);
     return true;
+
 }
